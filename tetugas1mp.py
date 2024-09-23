1b5d9f250b7a9294449d4c556ec346866eb62484
deylia <shgkdew@gmail.com>
2024-09-23 07:53:21 +0700

    Add files via upload

---
+++ tetugas1mp.py
@@ -0,0 +1,7 @@
+nama = input("nama kamu siapa? ")
+asal = input("dari mana asal kamu? ")
+sekolah = input("sebelumnya bersekolah dimana? ")
+
+print("halo selamat siang", nama)
+print("wah dari", asal)
+print("pasti menyenangkan di", sekolah)
---
+++ tugas2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 07:39:15 2024
+
+@author: Lenovo
+"""
+
+print("hitung luas ruangan:")
+panjang = float(input("panjang:"))
+lebar = float(input("lebar:"))
+satuan = input("inc/m:")
+hasil = panjang * lebar
+print("hasilnya adalah:" ,hasil,satuan)
